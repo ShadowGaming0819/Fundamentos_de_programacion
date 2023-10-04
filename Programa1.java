f8c038055a4dc992e8cf640e7a25c5424c58dae1
ShadowGaming0819 <146400564+ShadowGaming0819@users.noreply.github.com>
2023-10-03 19:55:39 -0600

    Add files via upload

---
+++ Programa1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa1;
+import java.lang.*;
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa1 {
+
+    public static void main(String[] args) {
+        String nombre = "Abraham Gonzalez";
+        System.out.println(nombre);
+        
+        //Librerias import 
+        //Proyecto package
+        //Inicio public class
+        //public state void main()
+        //Imprimir System.out.print(""); una sola linea 
+        //Imprimir System.out.println(""); salto de linea
+        
+    }
+}
