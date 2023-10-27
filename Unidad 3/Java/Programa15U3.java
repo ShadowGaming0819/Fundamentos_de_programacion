50316b646527a555256b55d57b11b3cbb4a82a2c
ShadowGaming0819 <146400564+ShadowGaming0819@users.noreply.github.com>
2023-10-26 21:54:04 -0400

    Add files via upload

---
+++ Unidad 3/Java/Programa15U3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa15u3;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa15U3 {
+
+    public static void main(String[] args) {
+        int c =1;
+        while(c<=20){
+            System.out.print(c);
+            c++;
+        
+        }
+    }
+}
