65779308c1fd5c1aac1d38e05ec82a70ed0346c8
ShadowGaming0819 <146400564+ShadowGaming0819@users.noreply.github.com>
2023-10-30 22:16:03 -0600

    Add files via upload

---
+++ Unidad 3/Java/Programa14U3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa14u3;
+
+/**
+ *
+ * @author Alexander Gonzalez
+ */
+public class Programa14U3 {
+
+    public static void main(String[] args) {
+        int contador1=1, contador2=2;
+        System.out.println("Programa para imprimir 100 numeros pares");
+        while(contador1 <= 100){
+        contador1=contador1+1;
+        System.out.println(contador2);
+        contador2= contador2 + 2;
+        }
+       
+    }
+}
